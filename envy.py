cc1284714c9cab25aec9f85cd38824159ff59512
9strebok <yastreb.ok.mag@gmail.com>
2020-11-05 23:41:20 +0300

    Code review

--- envy.py
+++ envy.py
@@ -7,31 +7,23 @@ from colorama import Fore, Style
 
 def get_longest_element_length(l: list):
     """
-    get_longest_element_length (l: list) -> int
+    get_longest_element_length (l: list[string]) -> int
     Return the length of the longest element in the list
     """
-    res = 0
-    for _ in l:
-        t = len(_)
-        if t > res:
-            res = t
-    return res
+    return max(len(el) for el in l)
 
 
 def nice_dict_print(d: dict):
     """
     nice_dict_print(d: dict) -> None
-
     Nice print a dictionary
     """
     k = d.keys()
     max_lenght = get_longest_element_length(k)
     for _ in k:
         dif = max_lenght - len(_)
-        if dif >= 0:
-            spaces = " " * dif
-            print(f"{Fore.CYAN}{spaces}{_}{Style.RESET_ALL}", d[_])
-
+        spaces = " " * dif
+        print(f"{Fore.CYAN}{spaces}{_}{Style.RESET_ALL}", d[_])
 
 
 def main():
