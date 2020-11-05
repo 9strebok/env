13d681b34c767f2a91e9856b14d2e7883ad349f1
9strebok <yastreb.ok.mag@gmail.com>
2020-11-06 00:05:20 +0300

    Update output

--- envy.py
+++ envy.py
@@ -13,7 +13,7 @@ def get_longest_element_length(l: list):
     return max(len(el) for el in l)
 
 
-def nice_dict_print(d: dict):
+def nice_dict_print(d: dict, str_length: int = 80):
     """
     nice_dict_print(d: dict) -> None
     Nice print a dictionary
@@ -22,11 +22,24 @@ def nice_dict_print(d: dict):
     max_lenght = get_longest_element_length(k)
     for _ in k:
         dif = max_lenght - len(_)
-        spaces = " " * dif
-        print(f"{Fore.CYAN}{spaces}{_}{Style.RESET_ALL}", d[_])
+        value_len = len(d[_])
+        if value_len <= str_length:
+            spaces = " " * dif
+            print(f"{Fore.CYAN}{spaces}{_}{Style.RESET_ALL}", d[_])
+        else:
+            spaces = " " * dif
+            new_str = f"{(max_lenght + 1) * ' '}"
+            print(f"{Fore.CYAN}{spaces}{_}{Style.RESET_ALL}")
+            for index, some in enumerate(d[_]):
+                if index % (str_length - 1) == 0 and index != 0:
+                    new_str +=  "\n" + (max_lenght + 1) * " " + some
+                else:
+                    new_str += some
+            print(new_str)
 
 
 def main():
+
     """Main function"""
     env = os.environ
     nice_dict_print(env)
